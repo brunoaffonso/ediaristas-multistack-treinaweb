bc578b66e1954e279746219707220e9606183587
brunoaffonso <brunoaffonso@gmail.com>
2021-06-16 18:59:40 -0300

    Componentes PageTitle e Subtitle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
-import Head from 'next/head'
-import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment'
+import Head from 'next/head';
+import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
+import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment';
 
 export default function Home() {
   return (
     <div>
       <SafeEnvironment />
+      <PageTitle
+        title={'Conheça os Profissionais'}
+        subtitle={
+          'Preencha seu endereço e veja todos os profissionais da sua localidade'
+        }
+      />
     </div>
-  )
+  );
 }
+
+//Continuar de 1:08:00
--- src/ui/components/feedback/SafeEnvironment/SafeEnvironment.tsx
+++ src/ui/components/feedback/SafeEnvironment/SafeEnvironment.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SafeEnvironmentContainer } from './SafeEnvironment.style';
-import { Container } from '@material-ui/core';
+import { Container } from '@material-ui/core'
 
 const SafeEnvironment = () => {
   return (
