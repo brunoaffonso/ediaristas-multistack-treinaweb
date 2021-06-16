51a7e660814cbae0ad65078fffebfec8e43a6457
brunoaffonso <brunoaffonso@gmail.com>
2021-06-16 18:04:01 -0300

    first commit

---
+++ src/ui/components/feedback/SafeEnvironment/SafeEnvironment.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeEnvironmentContainer } from './SafeEnvironment.style';
+import { Container } from '@material-ui/core';
+
+const SafeEnvironment = () => {
+  return (
+    <Container>
+      <SafeEnvironmentContainer>
+        Ambiente Seguro
+      </SafeEnvironmentContainer>
+    </Container>
+  );
+}
+
+export default SafeEnvironment;
